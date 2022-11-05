a224b331a9792fa4629558318e07d3cab7cd771f
Zahid <zahid@zmdev.com>
2022-11-05 20:35:19 +0000

    image generation working

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,5 +1,10 @@
 import { Configuration, OpenAIApi } from "openai";
 import got from "got";
+import fs from "fs";
+import _file from "../../public/assets/temp/fathippo.png";
+import path from "path";
+import https from "https";
+import Jimp from "jimp";
 
 const configuration = new Configuration({
     apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
@@ -18,14 +23,41 @@ export default async function Generate(req, res) {
 
     // const imageUrl = post.display_url;
     try {
-        const stream = got.stream("https://scontent-lcy1-2.cdninstagram.com/v/t51.2885-15/313899822_141806018598311_2046522328759021892_n.jpg?stp=dst-jpg_e35_s1080x1080&_nc_ht=scontent-lcy1-2.cdninstagram.com&_nc_cat=111&_nc_ohc=DoTP3uddRMcAX_jhTlI&edm=APU89FABAAAA&ccb=7-5&oh=00_AfDnJczWR5ZKFwCwhEMtgcdlkXV08uM5MCeo9E6CHdRwTw&oe=636AAF17&_nc_sid=86f79a");
+        const fileName = "308935607_1467103023763481_3239669449011058984_n";
+        const filePath = `https://instaigram.vercel.app/assets/temp/${fileName}.jpg`;
+        const savePath = `${process.cwd()}/public/uploads`;
 
+        const image = await Jimp.read(filePath);
+        image.cover(1000, 1000, Jimp.HORIZONTAL_ALIGN_LEFT | Jimp.VERTICAL_ALIGN_TOP);
+        await image.writeAsync(`${process.cwd()}/public/uploads/${fileName}.png`);
+        
+        const fileRead = fs.createReadStream(`${savePath}/${fileName}.png`);
 
-        const completion = await openai.createImageVariation(stream, 1, "1024x1024");
+        const completion = await openai.createImageVariation(
+            fileRead,
+            1,
+            "256x256"
+        );
         res.status(200).json({ result: completion.data });
     } catch (error) {
-        console.log(error.response.data.error);
+        console.log(error);
+        console.log(error.response.data);
         res.status(400).json({ error: "AI error" });
         return;
     }
+}
+
+
+async function getImage(url) {
+    return new Promise((resolve) => {
+        https.get(url, response => {
+            resolve(response);
+        })
+    })
+}
+
+async function pipeResponse(response, file) {
+    return new Promise(
+        (resolve) => response.pipe(file).on('close', () => resolve())
+    );
 }
